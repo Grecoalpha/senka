442ea6e1f9940e1fb677aea6fbbffe933386239f
aiden mcmanus <89177158+Grecoalpha@users.noreply.github.com>
2022-02-05 11:09:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const configuration = {
   SiteName: 'UNBL*CKED',
   Use2DTextOver3D: false, // Change to true if you want 2D over 3D
   SiteNameSize: 0.7, // Between 0 and +
-  NumberOfVerticalLines: 25,
+  NumberOfVerticalLines: 50,
   NumberOfDots: 5000,
   colors: {
     CanvasBackgroundColor: '#141414',
